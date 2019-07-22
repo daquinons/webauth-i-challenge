e797c457e0aef22440fb3f920713a323660ee713
David Quiñones <daquinons@users.noreply.github.com>
2019-07-22 13:56:23 +0200

    Create post to register

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const Users = require('./users/model');
 
 const server = express();
 
@@ -11,6 +12,20 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
+});
+
+server.post('/api/register', async (req, res, next) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 14);
+  try {
+    const response = await Users.create({
+      username,
+      password: hashedPassword
+    });
+    res.status(201).json(response);
+  } catch (error) {
+    next(new Error(error.message));
+  }
 });
 
 const port = process.env.PORT || 3000;
