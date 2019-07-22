42238166dc3ac19af540145fb7c7fcfabd2f9ba9
David Quiñones <daquinons@users.noreply.github.com>
2019-07-22 15:21:27 +0200

    Add implementation for custom bcrypt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const md5 = require('md5');
 const Users = require('./users/model');
 
 const server = express();
@@ -14,6 +15,20 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+const dummyBcrypt = {
+  hashSync: function(password, iterations = 10) {
+    let hashedPassword = password;
+    for (let i = 0; i < iterations; i++) {
+      hashedPassword = md5(hashedPassword);
+    }
+
+    return hashedPassword;
+  },
+  compareSync: function(password, hashToCompare, iterations = 10) {
+    return this.hashSync(password, iterations) === hashToCompare;
+  }
+};
+
 server.post('/api/register', async (req, res, next) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 14);
@@ -23,6 +38,19 @@ server.post('/api/register', async (req, res, next) => {
       password: hashedPassword
     });
     res.status(201).json(response);
+  } catch (error) {
+    next(new Error(error.message));
+  }
+});
+
+server.post('/api/register-dummy', async (req, res, next) => {
+  const { username, password } = req.body;
+  const hashedPassword = dummyBcrypt.hashSync(password, 14);
+  try {
+    res.json({
+      username,
+      password: hashedPassword
+    });
   } catch (error) {
     next(new Error(error.message));
   }
@@ -57,7 +85,6 @@ server.get('/api/restricted/users', async (req, res, next) => {
   }
 });
 
-
 async function checkCredentialsInBody(req, res, next) {
   try {
     const { username, password } = req.body;
@@ -71,7 +98,7 @@ async function checkCredentialsInBody(req, res, next) {
   } catch (error) {
     next(new Error(error.message));
   }
-};
+}
 
 async function restricted(req, res, next) {
   try {
@@ -85,12 +112,12 @@ async function restricted(req, res, next) {
         next();
       }
     } else {
-      res.status(401).json({ message: 'You are not identified'});
+      res.status(401).json({ message: 'You are not identified' });
     }
   } catch (error) {
     next(new Error(error.message));
   }
-};
+}
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
